333d0e99b2b29da099b27122aeb2d7ae98c2d130
Johnathan Silva <johnathans@apexglobal.co>
2025-05-15 21:50:37 -0500

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:pragma_exam/share/helpers/custom_scroll_behavior.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'config/constants/app_constants.dart';
+import 'config/constants/environment_constants.dart';
+import 'config/router/app_router.dart';
+import 'config/theme/app_theme.dart';
+import 'core/infrastructure/db/session/shared_preferences.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  _setPreferredOrientations();
+  _initSharedPreferences();
+  await Environment.load();
+  usePathUrlStrategy();
+  runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+void _setPreferredOrientations() => SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+void _initSharedPreferences() async {
+  final SharedPreferences sharedPreferences = SharedPreferences();
+  await sharedPreferences.init();
+}
+
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-       child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      title: kAppName,
+      theme: AppTheme.light,
+      routerConfig: ref.watch(routerProvider),
+      scrollBehavior: CustomScrollBehavior(),
     );
   }
 }
