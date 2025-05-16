61ce29015207eb7666835f3c549b99b6c698ef03
Johnathan Silva <johnathans@apexglobal.co>
2025-05-15 21:44:03 -0500

    add theme content

---
+++ lib/config/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color kWhite = Color(0xFFFFFFFF);
+
+const Color kGrey = Color(0xFF999999);
+const Color kGrey02 = Color(0xFFF5F5F5);
+const Color kGrey03 = Color(0xFFE0E0E0);
+
+const Color kBlack = Color(0xFF404040);
