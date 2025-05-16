c2ca873cfd33a249ddc414dc0bd09f3e32504576
Johnathan Silva <johnathans@apexglobal.co>
2025-05-15 21:49:47 -0500

    add widget appbar

---
+++ lib/share/widgets/appbar/search_appbar/android/search_appbar_android.dart
@@ -0,0 +1,14 @@
+part of '../custom_search_appbar.dart';
+
+class SearchAppbarAndroid extends StatelessWidget {
+  final Widget? title;
+  final List<Widget>? actions;
+  final Color? bgColor;
+
+  const SearchAppbarAndroid({super.key, this.title, this.actions, this.bgColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(backgroundColor: bgColor, elevation: kNone, title: title, actions: actions);
+  }
+}
---
+++ lib/share/widgets/appbar/search_appbar/ios/search_appbar_ios.dart
@@ -0,0 +1,18 @@
+part of '../custom_search_appbar.dart';
+
+class SearchAppbarIos extends StatelessWidget {
+  final Widget? title;
+  final List<Widget>? actions;
+  final Color? bgColor;
+
+  const SearchAppbarIos({super.key, this.title, this.actions, this.bgColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoNavigationBar(
+      middle: title,
+      backgroundColor: bgColor,
+      trailing: actions != null ? Row(mainAxisSize: MainAxisSize.min, children: actions!) : null,
+    );
+  }
+}
